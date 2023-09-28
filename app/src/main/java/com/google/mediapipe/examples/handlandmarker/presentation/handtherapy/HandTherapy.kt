5c9c4759e42b332958508fc9362c9368b05fcf7d
Becky <becky514@163.com>
2023-09-28 18:03:54 +0800

    new

---
+++ app/src/main/java/com/google/mediapipe/examples/handlandmarker/presentation/handtherapy/HandTherapy.kt
@@ -0,0 +1,12 @@
+package com.google.mediapipe.examples.handlandmarker.presentation.handtherapy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.mediapipe.examples.handlandmarker.R
+
+class HandTherapy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hand_therapy)
+    }
+}
